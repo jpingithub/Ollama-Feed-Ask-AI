2621562a89823d808efecae0125c55d4ed6cfefa
Nagarjuna <47788509+jpingithub@users.noreply.github.com>
2026-05-05 10:35:45 +0530

    Delete feed-ask-ai directory

--- feed-ask-ai/src/main/java/com/rb/feed_ask_ai/repository/RagRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rb.feed_ask_ai.repository;
-
-import com.rb.feed_ask_ai.entity.RagEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RagRepository extends JpaRepository<RagEntity, Integer> {
-}
